2c1e2bdc2477ed47a92aaa9f6323905334d72dea
Swarnarka Das <swarnarkadas@gmail.com>
2023-07-18 22:58:20 +0530

    Create room model,route and controller

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -1,6 +1,37 @@
 const express = require("express");
 
+const Room = require("../models/Room");
+const createError = require("../utils/error");
+
+const {
+    createRoom,
+    updateRoom,
+    deleteRoom,
+    getRoom,
+    getRooms,
+   } = require("../controllers/room");
+   
+const { verifyAdmin } = require("../utils/verifyToken");
+
 const router = express.Router();
+
+//Create
+router.post("/:hotelid", verifyAdmin,  createRoom)
+
+//UPDATE
+router.put("/:id", verifyAdmin,  updateRoom)
+
+
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom)
+
+
+//GET
+router.get("/:id",getRoom)
+
+
+//GET ALL
+router.get("/", getRooms)
 
 
 module.exports = router;
